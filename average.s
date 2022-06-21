855dad02934346588984c50cb1e9bcec85e72aea
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2022-06-21 17:30:53 -0300

    salvando .s

---
+++ average.s
@@ -0,0 +1,20 @@
+.text 
+.globl main
+
+main: 
+	li $t0, 1 # t0 = 1, valor inicial do fatorial
+	
+	li $v0, 5 # Ler inteiro
+	syscall
+calculateFactorial:
+	mult $t0, $v0 # muiltiplica $t0 por $v0
+	mflo $t0 # armazenar primeiros 32 bits da operacao
+	addi $v0, $v0, -1 # $v0--
+	bnez $v0, calculateFactorial # se $v0 != 0, continua calculando o fatorial
+print:
+	move $a0, $t0 # seta o parametro da funcao para o print 
+	li $v0, 1 # imprimir inteiro
+	syscall
+end:
+	li $v0, 10 #terminar o programa
+	syscall	
