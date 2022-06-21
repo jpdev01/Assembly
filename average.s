d07ae4d0d3005f1139494ecf40ffb3a65712e9ce
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2022-06-21 17:37:28 -0300

    add s files

--- average.s
+++ average.s
@@ -1,20 +1,47 @@
-.text 
+.data
+breakLine: 
+	.asciiz "\n"
+errorMessage:
+	.asciiz "ERRO"
+
+.text
 .globl main
 
-main: 
-	li $t0, 1 # t0 = 1, valor inicial do fatorial
-	
+main:
+	li $t0, -1 # t0 = -1, entrada para finalizar a aplicacao
+input:
 	li $v0, 5 # Ler inteiro
 	syscall
-calculateFactorial:
-	mult $t0, $v0 # muiltiplica $t0 por $v0
-	mflo $t0 # armazenar primeiros 32 bits da operacao
-	addi $v0, $v0, -1 # $v0--
-	bnez $v0, calculateFactorial # se $v0 != 0, continua calculando o fatorial
+calculate:
+	beq $v0, $t0, avarage # se $t0 == $t1 pula para calcular a media
+	add  $t1, $t1, $v0 # soma total
+	addi $t2, $t2, 1 # quantidade de valores digitados
+	
+	jal input # jump para entrada de valores
+avarage: # calcular media e armazena em t3
+	move $t3, $t1
+	div $t3, $t2
+	mflo $t3
 print:
-	move $a0, $t0 # seta o parametro da funcao para o print 
-	li $v0, 1 # imprimir inteiro
+	move $a0, $t1
+	li $v0, 1
+	syscall # imprime a soma de valores
+	
+	la $a0, breakLine
+	li $v0, 4 # quebra de linha
+	syscall
+	
+	beqz $t2, printError # se quantidade = 0, vai para printError
+	move $a0, $t3
+	li $v0, 1
+	syscall # imprime media
+	
+	jal end # finaliza o programa
+printError:
+	la $a0, errorMessage # set errorMessage para parametr de print string
+	li $v0, 4 # imprime string
 	syscall
 end:
-	li $v0, 10 #terminar o programa
-	syscall	
+	li $v0, 10 # encerra o programa
+	syscall
+	
